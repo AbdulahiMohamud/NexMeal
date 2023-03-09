f9041ecf60bdc230221b6d9a13e6e3c02216bac4
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-03-08 16:40:35 -0800

    api code is populating man page added ability to excude certain foods from recipes

--- src/components/RecipeSearch.js
+++ src/components/RecipeSearch.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import parse from 'html-react-parser';
 export default function RecipeSearch() {
+
+    const parse = require('html-react-parser');
+
     const [query , setQuery] = useState('');
+    const [excludeIngredients, setExcludeIngredients] = useState('');
     const [recipes, setRecipes] = useState([]);
 
 
 
         async function fetchRecipes() {
-            const response = await axios.get(`http://localhost:8080/recipes/search?query=${query}`);
+            const response = await axios.get(`http://localhost:8080/recipes/search?query=${query}&excludeIngredients=${excludeIngredients}`);
             setRecipes(response.data);
         }
         
@@ -17,6 +21,10 @@ export default function RecipeSearch() {
      function handleQueryChange (event) {
         setQuery(event.target.value);
 
+    }
+
+    function handleExcludeIngredients (e) {
+        setExcludeIngredients(e.target.value)
     }
     function handleSubmit (event) {
         event.preventDefault();
@@ -29,6 +37,7 @@ export default function RecipeSearch() {
         <div>
             <form onSubmit={handleSubmit}>
             <input type='text' value={query} onChange={handleQueryChange} />
+            <input type='text' value={excludeIngredients} onChange={handleExcludeIngredients} />
             <button type="submit">Search</button>
             </form>
             
@@ -38,7 +47,7 @@ export default function RecipeSearch() {
                         <h2>{recipe.title}</h2>
                         <img src={recipe.image} alt={recipe.titl} />
                         <p>Ready in {recipe.readyInMinutes} minutes</p>
-                        <p>Servings: {recipe.servings}</p>
+                        <p >Summary{parse(recipe.summary)}</p>
                         <a href={recipe.sourceUrl}>See the recipe</a>
                     </li>
                 ))}
