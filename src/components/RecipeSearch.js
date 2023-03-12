31b7102654322065df3b3148f1a6c5ca8856d80f
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-03-11 20:18:49 -0800

    added modal to show ingredients and add CSS styling for it

--- src/components/RecipeSearch.js
+++ src/components/RecipeSearch.js
@@ -1,86 +1,92 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 import parse from 'html-react-parser';
+import Modal from 'react-modal';
+import '/Users/abdulahimohamud/IdeaProjects/mayf-front/src/Css/RecipeSearch.css';
+
+Modal.setAppElement('#root');
+
 export default function RecipeSearch() {
+  const [query, setQuery] = useState('');
+  const [excludeIngredients, setExcludeIngredients] = useState('');
+  const [recipes, setRecipes] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
 
-    const parse = require('html-react-parser');
+  async function fetchRecipes() {
+    const response = await axios.get(`http://localhost:8080/recipes/search?query=${query}&excludeIngredients=${excludeIngredients}`);
+    setRecipes(response.data);
+  }
 
-    const [query , setQuery] = useState('');
-    const [excludeIngredients, setExcludeIngredients] = useState('');
-    const [recipes, setRecipes] = useState([]);
-    const [showDetails, setShowDetails] = useState (false);
-    const [selectedRecipe, setSelectedRecipe] = useState(null);
+  async function handleRecipeClick(recipe) {
+    setSelectedRecipe(recipe);
+    setModalIsOpen(true);
+  }
 
+  function handleCloseDetail() {
+    setSelectedRecipe(null);
+    setModalIsOpen(false);
+  }
 
-        async function fetchRecipes() {
-            const response = await axios.get(`http://localhost:8080/recipes/search?query=${query}&excludeIngredients=${excludeIngredients}`);
-            setRecipes(response.data);
-        }
+  function handleQueryChange(event) {
+    setQuery(event.target.value);
+  }
 
-        async function handleRecipeClick (recipeId) {
-            const response = await axios.get(`http://localhost:8080/recipes/${recipeId}`);
-            setSelectedRecipe(response.data);
-            setShowDetails(true);
-        }
-        function handleCloseDetail () {
-            setSelectedRecipe(null);
-            setShowDetails(false);
-        }
-        
-    
+  function handleExcludeIngredients(event) {
+    setExcludeIngredients(event.target.value);
+  }
 
-     function handleQueryChange (event) {
-        setQuery(event.target.value);
-
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (query) {
+      fetchRecipes();
     }
+  }
 
-    function handleExcludeIngredients (e) {
-        setExcludeIngredients(e.target.value)
-    }
-    function handleSubmit (event) {
-        event.preventDefault();
-        if(query){
-            fetchRecipes();
-            debugger;
-        }
-        
-    }
+  return (
+    <div className="RecipeSearch">
+      <h1 className="heading">Recipe Search</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="query" className="label">
+          Search for recipes:
+          <input type="text" id="query" value={query} onChange={handleQueryChange} className="input" />
+        </label>
+        <label htmlFor="excludeIngredients" className="label">
+          Exclude ingredients:
+          <input type="text" id="excludeIngredients" value={excludeIngredients} onChange={handleExcludeIngredients} className="input" />
+        </label>
+        <button type="submit" className="button">Search</button>
+      </form>
 
-    return(
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type='text' value={query} onChange={handleQueryChange} />
-                <input type='text' value={excludeIngredients} onChange={handleExcludeIngredients} />
-                <button type="submit">Search</button>
-            </form>
-            
-            <ul>
-                {recipes.map((recipe) => (
-                    <li key={recipe.id}>
-                        <h2>{recipe.title}</h2>
-                        <img src={recipe.image} alt={recipe.titl} />
-                        <p>Ready in {recipe.readyInMinutes} minutes</p>
-                        <p >Summary{parse(recipe.summary)}</p>
-                        <button onClick={() => handleRecipeClick(recipe.id)}>See the recipe</button>
-                    </li>
-                ))}
-            </ul>
+      <ul className="recipes">
+        {recipes.map((recipe) => (
+          <li key={recipe.id} className="recipe">
+            <h2 className="recipe__title">{recipe.title}</h2>
+            <img src={recipe.image} alt={recipe.title} className="recipe__image" />
+            <p className="recipe__time">Ready in {recipe.readyInMinutes} minutes</p>
+            <p className="recipe__summary">{parse(recipe.summary)}</p>
+            <button onClick={() => handleRecipeClick(recipe)} className="recipe__button">See the recipe</button>
+          </li>
+        ))}
+      </ul>
 
-            {showDetails && (
-                <div>
-                    <h2>{selectedRecipe.title}</h2>
-                    <img src={selectedRecipe.image} alt={selectedRecipe.title} />
-                    <p>Ready in {selectedRecipe.readyInMinutes} minutes</p>
-                    <p>{parse(selectedRecipe.summary)}</p>
-                    <button onClick={handleCloseDetail}>Close</button>
-                    <h3>Instructions:</h3>
-                    <ol>
-                        {selectedRecipe.analyzedInstructions[0].steps.map((step) => (
-                            <li key={step.number}>{step.step}</li>
-                        ))}
-                    </ol>
-                </div>
-            )}
-        </div>
-    )
+      <Modal isOpen={modalIsOpen} onRequestClose={handleCloseDetail} className="modal" overlayClassName="overlay">
+        {selectedRecipe && (
+          <div>
+            <h2 className="modal__title">{selectedRecipe.title}</h2>
+            <img src={selectedRecipe.image} alt={selectedRecipe.title} className="modal__image" />
+            <p className="modal__time">Ready in {selectedRecipe.readyInMinutes} minutes</p>
+
+            <button onClick={handleCloseDetail} className="modal__close-button">Close</button>
+            <h3 className="modal__instruction-title">Instructions:</h3>
+            <ol className="modal__instruction-list">
+              {selectedRecipe.analyzedInstructions[0].steps.map((step) => (
+                <li key={step.number} className="modal__instruction-step">{step.step}</li>
+              ))}
+            </ol>
+          </div>
+        )}
+      </Modal>
+    </div>
+  );
 }
