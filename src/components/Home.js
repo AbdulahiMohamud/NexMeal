ed973d5d90285dff54e36ddf719f694bbd53b5bc
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-03-22 16:12:12 -0700

    added css styling to the nav bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,14 @@
 import React, { useEffect, useState } from "react";
 
 export default function Home(){
-    const [data, setData] = useState([]);
-
-    useEffect( () => {
-        fetch('http://localhost:8080/api/data')
-        .then(Response => Response.json)
-        .then(data => setData(data))
-        .catch(error => console.error({error}))
-
-    } )
+    
 
 
 
     return (
         <div>
       <h1>HomePage</h1>
-      <ul>
-        {data.map(item => (
-          <li key={item.id}>{item.name} ({item.email})</li>
-        ))}
-      </ul>
+      
     </div>
 
     )
