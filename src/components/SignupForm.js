df30576c142bc3bbe95058c4a592946c226fc489
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-04-04 00:41:04 -0700

    fixed the log to allow user to log in with username or password and for sign up to deny if the email or username already exists in the database

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,72 +1,96 @@
 import React, { useState } from 'react';
+import { useNavigate  } from 'react-router-dom';
+
 import '/Users/abdulahimohamud/IdeaProjects/mayf-front/src/Css/SignupForm.css';
 
 
 export default function SignupForm() {
-  const [userName, setUserName] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    let  navigate = useNavigate();
+    const [error, setError] = useState('');
+    const [userName, setUserName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log({ userName, firstName, lastName, email, password });
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log({ userName, firstName, lastName, email, password });
 
-    // Send POST request to Spring Boot API
-    fetch('http://localhost:8080/api/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ userName, firstName, lastName, email, password })
-    })
+        // Send POST request to Spring Boot API
+        fetch('http://localhost:8080/api/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ userName, firstName, lastName, email, password })
+        })
 
-    .then(response => {
-        console.log({response});
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error(error.message));
+        .then(response => {
+            console.log({response});
+            if (!response.ok) {
+                if (response.status === 409) {
+                } else {
+                  throw new Error(response.statusText);
+                }
+              } else {
+                navigate('/login');
+              }
+              return response.json();
+        })
 
-    // Clear form fields
-    setUserName('');
-    setFirstName('');
-    setLastName('');
-    setEmail('');
-    setPassword('');
-  };
+        .then(data => {
+            console.log(data);
+        })
 
-  return (
-    <><h1>Signup</h1><form onSubmit={handleSubmit}>
-          <label>
-              UserName:
-              <input type="text" value={userName} onChange={event => setUserName(event.target.value)} />
-          </label>
-          <br />
-          <label>
-              FirstName:
-              <input type="text" value={firstName} onChange={event => setFirstName(event.target.value)} />
-          </label>
-          <br />
-          <label>
-              LastName:
-              <input type="text" value={lastName} onChange={event => setLastName(event.target.value)} />
-          </label>
-          <br />
-          <label>
-              Email:
-              <input type="email" value={email} onChange={event => setEmail(event.target.value)} />
-          </label>
-          <br />
-          <label>
-              Password:
-              <input type="password" value={password} onChange={event => setPassword(event.target.value)} />
-          </label>
-          <br />
-          <button type="submit">Sign Up</button>
-      </form></>
-  );
+        .catch(error => {
+            console.error({error});
+            if (error.message.includes("Username")){
+                setError("The Username already exists"); 
+            } else if (error.message.includes("Email")){
+                setError("The Email is already in use"); 
+            }
+            
+        });
+
+        // Clear form fields
+        setUserName('');
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPassword('');
+    };
+
+    return (
+        <>
+            <h1>Signup</h1>
+            {error && <div className="error">{error}</div>}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    UserName:
+                    <input type="text" value={userName} onChange={event => setUserName(event.target.value)} />
+                </label>
+                <br />
+                <label>
+                    FirstName:
+                    <input type="text" value={firstName} onChange={event => setFirstName(event.target.value)} />
+                </label>
+                <br />
+                <label>
+                    LastName:
+                    <input type="text" value={lastName} onChange={event => setLastName(event.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Email:
+                    <input type="email" value={email} onChange={event => setEmail(event.target.value)} />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input type="password" value={password} onChange={event => setPassword(event.target.value)} />
+                </label>
+                <br />
+                <button type="submit">Sign Up</button>
+            </form>
+        </>
+    );
 }
-
